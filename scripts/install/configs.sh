bf892284372befd087021a56076993ff51dbf9b3
Alexander Alanakyan <73045206+alexanderalanakyan@users.noreply.github.com>
2026-05-16 15:30:22 -0400

    Create necessary directories and configuration files

--- scripts/install/configs.sh
+++ scripts/install/configs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+mkdir -p /mnt/etc/systemd/sytem
 cat > /mnt/etc/systemd/system/write-cache-disabler.service <<EOF
 [Unit]
 Description=Write cache disabler daemon
@@ -13,7 +13,7 @@ ExecStart=/usr/local/sbin/write-cache-disabler
 WantedBy=multi-user.target
 EOF
 
-
+mkdir -p /mnt/usr/local/sbin/
 cat > /mnt/usr/local/sbin/write-cache-disabler <<EOF
 #!/bin/sh
 set -e
@@ -32,7 +32,7 @@ done
 EOF
 
 chmod +x /mnt/usr/local/sbin/write-cache-disabler
-
+mkdir -p /mnt/etc/sysctl.d/
 cat > /mnt/etc/sysctl.d/99-vm-zram-parameters.conf <<EOF
 vm.swappiness = 180
 
@@ -42,24 +42,21 @@ vm.watermark_scale_factor = 125
 
 vm.page-cluster = 0
 EOF
-
+mkdir -p /mnt/etc/systemd/
 cat > /mnt/etc/systemd/zram-generator.conf <<EOF
 [zram0]
 compression-algorithm = zstd lzo-rle
 EOF
-
+mkdir -p /mnt/etc/udev/rules.d/
 cat > /mnt/etc/udev/rules.d/69-hdparm.rules <<EOF
 ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd*", RUN+="/usr/bin/hdparm -B 254 -S 0 /dev/sda"
 EOF
-
+mkdir -p /mnt/etc/conf.d/
 cat > mnt/etc/conf.d/pacman-contrib <<EOF
  "PACCACHE_ARGS='-k2'"
 EOF 
 
-cat > /mnt/etc/modprobe.d/uvcvideo.conf <<EOF
-blacklist uvcvideo
-EOF
-
+mkdir -p /mnt/etc/NetworkManager/conf.d/
 cat > /mnt/etc/NetworkManager/conf.d/powersave.conf <<EOF
 [connection]
 wifi.powersave=2
